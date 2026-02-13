d33ccfac1cb6a9b305034b7825dcfa3f35e4bf33
AD\MSURahman <MSURahman@lmkr.com>
2026-02-13 12:48:44 +0500

    for change my pc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose=require('mongoose');
 const authRouter= require('./routers/authRouter');
 const activityLogger = require("./middlewares/logger.middleware");
 const authMiddleware = require("./middlewares/authMiddleware");
-
+const axios = require('axios');
 const app = express();
 
 app.use(cors());
@@ -32,6 +32,40 @@ app.get("/", (req, res) => {
   
   res.json({ message: "Hello from the server" });
 });
+
+
+app.post('/api/get-prediction', async (req, res) => {
+    try {
+
+        const fastApiResponse = await axios.post('http://127.0.0.1:8000/predict', {
+            review: req.body.review 
+        });
+
+     
+        const sentiment = fastApiResponse.data.sentiment;
+        const confidence = fastApiResponse.data.confidence;
+
+        console.log('Model Prediction:', sentiment);
+        console.log('Confidence:', confidence);
+
+     
+      res.json({ 
+            success: true, 
+            prediction: sentiment, 
+            confidence: confidence,
+            processedText: fastApiResponse.data.processed_text
+        });
+
+    } catch (error) {
+        console.error("Error communicating with FastAPI:", error.message);
+        
+        res.status(500).json({ 
+            success: false, 
+            error: "Model service is offline or returned an error" 
+        });
+    }
+});
+
 
 const PORT = process.env.PORT || 3000;
 
